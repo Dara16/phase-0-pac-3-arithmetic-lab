4b7204c0ac33815445489782df3e3b8b725ff44b
dara <just4oludee@yahoo.com>
2021-03-13 21:57:18 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/ b;
+}
+
+function increment(number) {
+    number++
+    return number
+
+}
+
+function decrement(number) {
+    number--
+    return number
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+    }
+    makeInt('0.2328');
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+
+
+
+
